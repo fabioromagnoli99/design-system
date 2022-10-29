2e3d77b71b965b5dd1449b7c2ead6c7b0bfa8eb9
Fabio <fabio.gromagnoli@gmail.com>
2022-10-29 13:48:50 +0100

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,12 +15,6 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION')
-      config.base = '/design-system/'
-  
-      return config
-    }
 
   
 }
